2e10a9b751be12af0f7c5af8149c8feed93bf0e0
manuelgtzb <ricardoemmanuel.gutierrezbetancourt.gen23@cbtis105.edu.mx>
2025-06-12 18:47:37 -0600

    terminado

--- Controllers/Api/ApiUsuariosController.cs
+++ Controllers/Api/ApiUsuariosController.cs
@@ -14,18 +14,36 @@ public class ApiUsuariosController : ControllerBase
 
   private readonly IMongoCollection<Usuario> collection;
   public ApiUsuariosController()
-{
-   var client = new MongoClient(CadenasConexion.MONGO_DB);
-   var database = client.GetDatabase("Escuela_Ricardo_Joselyn");
-   this.collection = database.GetCollection<Usuario>("Usuarios");
+  {
+    var client = new MongoClient(CadenasConexion.MONGO_DB);
+    var database = client.GetDatabase("Escuela_Ricardo_Joselyn");
+    this.collection = database.GetCollection<Usuario>("Usuarios");
 
-}
+  }
 
-    [HttpGet]
-    public IActionResult ListarUsuarios()
+  [HttpGet]
+  public IActionResult ListarUsuarios(string? texto)
+  {
+    var filter = FilterDefinition<Usuario>.Empty;
+    if (!string.IsNullOrWhiteSpace(texto))
     {
-        var filter = FilterDefinition<Usuario>.Empty;
-        var list = this.collection.Find(filter).ToList();
-        return Ok(list);
+      var filterNombre = Builders<Usuario>.Filter.Regex(u => u.Nombre, new BsonRegularExpression(texto, "i"));
+      var filterCorreo = Builders<Usuario>.Filter.Regex(u => u.Correo, new BsonRegularExpression(texto, "i"));
+      filter = Builders<Usuario>.Filter.Or(filterNombre, filterCorreo);
     }
+    var list = this.collection.Find(filter).ToList();
+    return Ok(list);
+  }
+
+[HttpDelete("{id}")]
+  public IActionResult Delete(string id)
+  {
+    var filter = Builders<Usuario>.Filter.Eq(x => x.Id, id);
+    var item = this.collection.Find(filter).FirstOrDefault();
+    if (item != null)
+    {
+      this.collection.DeleteOne(filter);
+    }
+    return NoContent();
+  }
 }
